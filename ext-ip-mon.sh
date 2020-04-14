b4a0cde6badb7e021a2f49967396df24602bf684
mutagenspree <54075280+mutagenspree@users.noreply.github.com>
2020-04-14 23:37:12 +0300

    Update ext-ip-mon.sh

--- ext-ip-mon.sh
+++ ext-ip-mon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 path="/path/to/file/with/last/ip"
-email="Where send Notifications"
+email="Where to send Notifications"
 
 ip1=`curl ifconfig.me`
 ip2=`cat < "$path"`
@@ -9,5 +9,5 @@ ip2=`cat < "$path"`
 if [ "$ip1" != "$ip2"]
   then
   echo "$ip1" > "$path"
-  echo "New IP is $ip1" | mail -s "Warninf IP was change" "$email
+  echo "New IP is $ip1" | mail -s "Warning IP has changed" "$email
 fi
